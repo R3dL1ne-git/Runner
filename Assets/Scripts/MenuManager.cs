ca31401e4730aeabdd3f56e97a87e08dcf049d11
R3dL1ne <andrey.fijatov.2013@gmail.com>
2022-11-15 23:14:18 +0300

    Adding menu game

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject recordsPanel;
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+    
+    public void RecordsPanel()
+    {
+        recordsPanel.SetActive(true);
+    }
+
+    public void ExitRecordsPanel()
+    {
+        recordsPanel.SetActive(false);
+    }
+}
